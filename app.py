c539edd06f90df030c73d649e40eb5f76531e23f
Felipe Santos <retr04rch@gmail.com>
2025-01-31 16:03:09 -0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from ChicO.functions import *
-
-def main():
-    while True:
-        audio = speech_recognition.listen()
-        requisicao = speech_recognition.comando(audio)
-        limpar_tela()
-
-main()
