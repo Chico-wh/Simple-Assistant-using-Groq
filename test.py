18fa31dbb26e788857a7b8cd9d68526ad5202032
Felipe Santos <retr04rch@gmail.com>
2025-01-31 15:38:33 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-url = ""
-api_key = "api_live_UVer4qTOKVZy6bi1Xebhe01SQzocf4VaKjRJSdAZADKcYog5qhvG7Z"
 
-import requests as rq 
-
-data = rq.get(f'https://api.apitube.io/v1/news/everything?per_page=50&api_key={api_key}')
-
-print(data.json())
